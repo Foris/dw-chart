699c72e9ce1e90d49da3b6dc39b9caaf85a1304c
daniel-llach <daniel.llach@gmail.com>
2016-05-19 18:13:49 -0300

    chart barpie

--- component/dw-chart.js
+++ component/dw-chart.js
@@ -53,20 +53,49 @@
       // take data objects and updatebar
       let data = options['data'];
 
-      // options each
-      data.forEach(function (data, i) {
-        let itemId = data['title'];
-        // hide legends
-        $el.find('.legend').fadeOut("slow");
-        // set bar length
-        let $item = $el.find('li[data-id="' + itemId + '"]');
-        let number = data['number'].toFixed(options['decimals']);
-        methods.updateBar($item.find('.bar'),number, options['unit']);
-      });
+      // update depend of type
+      switch(options.type){
+        case 'bar':
+          // options each
+          data.forEach(function (data, i) {
+            let itemId = data['title'].replace(' ','');
+            console.log("itemId: ", itemId);
+            // hide legends
+            $el.find('.legend').fadeOut("slow");
+            // set bar length
+            let $item = $el.find('li[data-id="' + itemId + '"]');
+            let number = data['number'].toFixed(options['decimals']);
+            methods.updateBar($item.find('.bar'),number, options['unit']);
+          });
+          break;
+        case 'barpie':
+          // hide numbers
+          let legends = $el.find('> content > .chart > .bottom > li > span > .legend');
+          legends.fadeOut("slow");
+          // calculate total sum of numbers
+          let sum = 0;
+          data.forEach(function (dat, i) {
+            sum = sum + dat['number'];
+            // change text
+            $el.find('> content > .chart > .bottom > li:eq(' + i + ') > span > .legend').text(dat['number']);
+          });
+          // update width
+          methods.updateBarpie($el, sum);
+          // set height
+          let barHeight = $el.find('> content > .chart > .bottom > li:first-child').outerHeight();
+          $el.find('> content > .chart > .bottom').css({
+            height: barHeight + 13 + 'px'
+          })
+          // show legends
+          legends.delay(1500).fadeIn('slow');
+          $el.find('> content > .chart > .bottom > li > .legend').delay(1500).fadeIn('slow');
+          break;
+      }
+
+      // updateData = false;
     },
 
     deployComponent: function($el, options){
-      // convert the div into a dw-filter component
       $el.addClass('dw-chart');
     },
 
@@ -84,10 +113,9 @@
       let template = _.template(templateContent);
       $el.html( template({
         id: options['name'],
-        title: options['title']
+        title: options['title'],
+        type: options.type
       }) );
-
-      // $el.addClass(options['style']);
 
       if (typeof options !== 'undefined') {
         methods.itemTemplate($el, options)
@@ -98,6 +126,9 @@
       switch(options.type){
         case 'bar':
           methods.barTemplate($el, options);
+          break;
+        case 'barpie':
+          methods.barpieTemplate($el, options);
           break;
       }
     },
@@ -113,7 +144,7 @@
 
         // options each
         data.forEach(function (data, i) {
-          let itemId = data['title'];
+          let itemId = data['title'].replace(' ','');
           let contentHtml = template({
             id: itemId,
             title: data['title'],
@@ -129,6 +160,83 @@
           methods.updateBar($item.find('.bar'), data['number'], options['unit']);
         });
       });
+    },
+    barpieTemplate: function($el, options){
+      let datas = options['data'];
+      // orientation
+      let orientation = options['orientation'];
+      // put items
+      let template = 'templates/barpie-item.html';
+
+      $.get(urlBase + template, function( result ) {
+        let template = _.template(result);
+
+        // options each
+        datas.forEach(function (data, i) {
+          let itemId = data['title'].replace(' ','');
+          let contentHtml = template({
+            id: itemId,
+            title: data['title'],
+            number: data['number'],
+            unit: options.unit,
+            decimals: options.decimals,
+            emphasis: options.emphasis
+          });
+          // paint it
+          $el.find('content > .chart > .bottom').append(contentHtml);
+          // set bar length
+          let $item = $el.find('li[data-id="' + itemId + '"]');
+
+          methods.initBarpie($el, $item.find('.barpie'), data['number'], options['unit'], datas.length);
+        });
+      }).done(function(){
+        // calculate total sum of numbers
+        let sum = 0;
+        datas.forEach(function (data, i) {
+          sum = sum + data['number'];
+        });
+        // update width
+        methods.updateBarpie($el, sum);
+        // set height
+        let barHeight = $el.find('> content > .chart > .bottom > li:first-child').outerHeight();
+        $el.find('> content > .chart > .bottom').css({
+          height: barHeight + 13 + 'px'
+        })
+        // show legends
+        $el.find('> content > .chart > .bottom > li > span > .legend').delay(1500).fadeIn('slow');
+        $el.find('> content > .chart > .bottom > li > .legend').delay(1500).fadeIn('slow');
+      });
+    },
+    initBarpie: function($el, $item, num, unit, total){
+      // set start item length
+      $el.find('li.barpie').css({
+        width: 'calc(' + (100/total) + '%)'
+      });
+    },
+    updateBarpie: function($el, sum){
+      let items = $el.find('li.barpie');
+      _.each(items, function(item, i){
+        let $item = $(item);
+        let num = $item.find('> .barpie > .legend').text();
+        num = parseInt(num);
+        let perc = methods.getPercentage(num, sum);
+        let bgColor = methods.percentageColor(perc+25);
+
+        $item.delay(500).animate({
+          width: perc + '%',
+          backgroundColor: bgColor
+        }, {
+          duration: 1000,
+          easign: {
+            width: "easeout",
+            backgroundColor: "easeout"
+          },
+          complete: function() {
+            // // Animation complete.
+          }
+        });
+
+      })
     },
     updateBar: function($item, num, unit){
       let bgColor = methods.percentageColor(num);
@@ -188,6 +296,10 @@
 
       var hslaText = "hsl(" + H + "," + S + "," + L + ")";
       return hslaText;
+    },
+    getPercentage: function(num, sum){
+      let perc = num * 100 / sum;
+      return perc;
     }
   }
 
@@ -195,124 +307,124 @@
   // Events
   var events = {
 
-    startOrder: function($el, options){
-      if(!options.add){
-        // sortable
-        let sortable = options.sortable;
-        var containerEl = $el.find('.items').first()[0];
-
-        if(sortable){
-          Sortable.create(containerEl, {});
-          events.dragItemsOrder($el, options);
-        }else{
-          Sortable.create(containerEl, {});
-          $el.find('.item > .left').remove();
-        }
-      }
-      events.dragItemsOrder($el, options);
-      methods.updatePosition($el);
-      events.removeItem($el, options);
-
-    },
-    dragItemsOrder: function($el, options){
-      let $items = $el.find('.items .item');
-
-      $items.bind({
-        dragstart: function(event){
-          $(event.target).addClass('indicator');
-        },
-        dragenter: function(event){
-          $to = $(event.target).data('id');
-          methods.updatePosition($el);
-        },
-        dragover: function(event){
-        },
-        dragend: function(event){
-          $(event.target).removeClass('indicator');
-          api.val($el); // trigger update ids
-        },
-        drop: function(event){
-        }
-      })
-    },
-    startSelect: function($el, options){
-      events.clickItemsSelect($el, options);
-      methods.updatePosition($el);
-      events.removeItem($el, options);
-    },
-    dragItemsOrder: function($el, options){
-      let $items = $el.find('.items .item');
-
-      $items.bind({
-        dragstart: function(event){
-          $(event.target).addClass('indicator');
-        },
-        dragenter: function(event){
-          $to = $(event.target).data('id');
-          methods.updatePosition($el);
-        },
-        dragover: function(event){
-        },
-        dragend: function(event){
-          $(event.target).removeClass('indicator');
-          api.val($el); // trigger update ids
-        },
-        drop: function(event){
-        }
-      })
-    },
-    clickItemsSelect: function($el, options){
-      let $items = $el.find('.items .item');
-
-      $items.bind({
-        click: function(event){
-          let itemId = $(event.target).data('id');
-          $items.removeClass('selected');
-          $(event.target).addClass('selected');
-
-          $el.trigger('change', itemId);
-        }
-      })
-    },
-    clickOut: function($el, options){
-      let $items = $el.find('content > .items');
-      let $clear = $el.find('.clear');
-      $(document).mouseup(function (e)
-      {
-          if (!$el.is(e.target) // if the target of the click isn't the $el...
-              && $el.has(e.target).length === 0) // ... nor a descendant of the $el
-          {
-              $items.addClass('hide');
-              $clear.addClass('hide')
-          }
-      });
-    },
-    removeItem: function($el, options){
-      let $rm = $el.find('.remove');
-
-      // For each item, schedule an event for deletion
-      // This event is attached only once, so it doesn't trigger
-      // multiple times. An attribute in the data of the element
-      // is used to track the scheduling.
-      $.each($rm, function ($index, item) {
-        var $item = $(item);
-
-        var scheduledToDelete = $item.data('scheduledToDelete');
-        if (!scheduledToDelete) {
-          $item.on("click", function(event){
-            event.preventDefault();
-            event.stopPropagation();
-            let $this = $(event.target);
-            $this.parent().remove();
-            api.val($el);
-            // trigger remove event and pass item id
-            $el.trigger('delete', $(event.target).parent().data('id'));
-          });
-
-          $item.data('scheduledToDelete', true);
-        }
-      });
-    }
+    // startOrder: function($el, options){
+    //   if(!options.add){
+    //     // sortable
+    //     let sortable = options.sortable;
+    //     var containerEl = $el.find('.items').first()[0];
+    //
+    //     if(sortable){
+    //       Sortable.create(containerEl, {});
+    //       events.dragItemsOrder($el, options);
+    //     }else{
+    //       Sortable.create(containerEl, {});
+    //       $el.find('.item > .left').remove();
+    //     }
+    //   }
+    //   events.dragItemsOrder($el, options);
+    //   methods.updatePosition($el);
+    //   events.removeItem($el, options);
+    //
+    // },
+    // dragItemsOrder: function($el, options){
+    //   let $items = $el.find('.items .item');
+    //
+    //   $items.bind({
+    //     dragstart: function(event){
+    //       $(event.target).addClass('indicator');
+    //     },
+    //     dragenter: function(event){
+    //       $to = $(event.target).data('id');
+    //       methods.updatePosition($el);
+    //     },
+    //     dragover: function(event){
+    //     },
+    //     dragend: function(event){
+    //       $(event.target).removeClass('indicator');
+    //       api.val($el); // trigger update ids
+    //     },
+    //     drop: function(event){
+    //     }
+    //   })
+    // },
+    // startSelect: function($el, options){
+    //   events.clickItemsSelect($el, options);
+    //   methods.updatePosition($el);
+    //   events.removeItem($el, options);
+    // },
+    // dragItemsOrder: function($el, options){
+    //   let $items = $el.find('.items .item');
+    //
+    //   $items.bind({
+    //     dragstart: function(event){
+    //       $(event.target).addClass('indicator');
+    //     },
+    //     dragenter: function(event){
+    //       $to = $(event.target).data('id');
+    //       methods.updatePosition($el);
+    //     },
+    //     dragover: function(event){
+    //     },
+    //     dragend: function(event){
+    //       $(event.target).removeClass('indicator');
+    //       api.val($el); // trigger update ids
+    //     },
+    //     drop: function(event){
+    //     }
+    //   })
+    // },
+    // clickItemsSelect: function($el, options){
+    //   let $items = $el.find('.items .item');
+    //
+    //   $items.bind({
+    //     click: function(event){
+    //       let itemId = $(event.target).data('id');
+    //       $items.removeClass('selected');
+    //       $(event.target).addClass('selected');
+    //
+    //       $el.trigger('change', itemId);
+    //     }
+    //   })
+    // },
+    // clickOut: function($el, options){
+    //   let $items = $el.find('content > .items');
+    //   let $clear = $el.find('.clear');
+    //   $(document).mouseup(function (e)
+    //   {
+    //       if (!$el.is(e.target) // if the target of the click isn't the $el...
+    //           && $el.has(e.target).length === 0) // ... nor a descendant of the $el
+    //       {
+    //           $items.addClass('hide');
+    //           $clear.addClass('hide')
+    //       }
+    //   });
+    // },
+    // removeItem: function($el, options){
+    //   let $rm = $el.find('.remove');
+    //
+    //   // For each item, schedule an event for deletion
+    //   // This event is attached only once, so it doesn't trigger
+    //   // multiple times. An attribute in the data of the element
+    //   // is used to track the scheduling.
+    //   $.each($rm, function ($index, item) {
+    //     var $item = $(item);
+    //
+    //     var scheduledToDelete = $item.data('scheduledToDelete');
+    //     if (!scheduledToDelete) {
+    //       $item.on("click", function(event){
+    //         event.preventDefault();
+    //         event.stopPropagation();
+    //         let $this = $(event.target);
+    //         $this.parent().remove();
+    //         api.val($el);
+    //         // trigger remove event and pass item id
+    //         $el.trigger('delete', $(event.target).parent().data('id'));
+    //       });
+    //
+    //       $item.data('scheduledToDelete', true);
+    //     }
+    //   });
+    // }
   };
 
   // jquery component stuff
